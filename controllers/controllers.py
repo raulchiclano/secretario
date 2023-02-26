bc9371aa1975c5cd1b16b143b79d254b34e6dcfd
raulchiclano <raulchiclano@gmail.com>
2023-02-26 20:13:34 +0100

    Subida inicial

---
+++ controllers/controllers.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from datetime import *
+from odoo.http import request
+from werkzeug.utils import redirect
+
+
+def check_password(password, expected_password):
+    return password == expected_password
+
+def auth_function(self, **kwargs):
+    password = kwargs.get('password')
+    if not password:
+        return False
+    return Pendientes.check_password(password, 'secret_password')
+
+class Test (http.Controller):
+
+    @http.route('/demasiadopronto', auth='public')
+    def index(self, **kw):
+        return http.request.render("secretary.demasiadopronto")
+    
+
+
+class Panel(http.Controller):
+    #En desuso
+    @http.route('/informar/', auth='public')
+    def index(self, **kw):
+        today = date.today().day
+        Grupos = http.request.env['secretary.grupos']
+        Publicadores = http.request.env['secretary.publicadores']
+        if today >31:
+            return redirect('/demasiadopronto')
+        else:
+            return http.request.render('secretary.index', {
+                'grupos': Grupos.search([]),
+                'publicadores': Publicadores.search([]),
+        })
+
+    # EL ACTUAL
+    @http.route('/enviarinforme/', auth='public')
+    def index2(self, **kw):
+        today = date.today().day
+        Grupos = http.request.env['secretary.grupos']
+        Publicadores = http.request.env['secretary.publicadores']
+        if today >15:
+            return redirect('/demasiadopronto')
+        else:
+            return http.request.render('secretary.enviarinforme', {
+                'grupos': Grupos.search([]),
+                'publicadores': Publicadores.search([('activo', '=', True),]),
+        })
+
+    @http.route(['/panel/submit/'], type='http', auth="public", website=True)
+    def informes_form_submit(self, **post):
+        mes = Panel.calculoMesAInformar()[0]
+        año = Panel.calculoMesAInformar()[1]
+        año_default= str(date.today().year)
+        #print("valor de mes_anterior--------------------->",mes, flush=True)
+        informes = http.request.env['secretary.informes'].sudo().create({
+            'nombre': post.get('publicador'), 
+            'horas': post.get('horas'),
+            'mes' : mes,
+            'año' : año_default,
+            'revisitas': post.get('revisitas'),
+            'cursos' : post.get('cursos'),
+            'videos' : post.get('videos'),
+            'publicaciones' : post.get('publicaciones'),
+            'tipo_informe' : post.get('tipo_informe'),
+            'año' : año,
+            'notas' : post.get('notas'),
+            
+        })
+        vals = {
+            'informes': informes,
+        }
+        #inherited the model to pass the values to the model from the form#
+        return http.request.render("secretary.success", vals)
+        #finally send a request to render the thank you page#
+    
+    def calculoMesAInformar():
+        
+        mes_anterior= str(date.today().month)
+        año = datetime.strftime(datetime.today(),'%Y')
+        
+        if mes_anterior == '1':
+           mes_anterior = '12'
+           año = str(int(año)-1)
+        else:
+            mes_anterior = str((date.today().month)-1)
+        fecha = (mes_anterior, año)    
+        return fecha
+
+
+
+class Pendientes (http.Controller):
+    
+    @http.route(['/password/'], auth="public")
+    def informes_form_submit(self, **post):
+        
+       
+        #inherited the model to pass the values to the model from the form#
+        return http.request.render("secretary.password")
+        #finally send a request to render the thank you page#
+
+    @http.route('/pendientes', type='http', auth="public", website=True, csrf=False)
+    def pendientes(self, **post):
+        fecha= '%s-%s' % (Panel.calculoMesAInformar()[0], Panel.calculoMesAInformar()[1])
+        Grupos = http.request.env['secretary.grupos']
+        Publicadores_conInforme = http.request.env['secretary.publicadores'].search([('informe_id.fecha','=',fecha)])
+        Publicadores_total = http.request.env['secretary.publicadores'].search([('activo', '=', 'TRUE')])
+        Publicadores = Publicadores_total - Publicadores_conInforme
+        Mensaje = "Querido hermano, solo recordarte que todavía no hemos recibido tu informe. Por favor, envíalo a la mayor brevedad posible. ¡Muchas gracias!%0APicha aquí --> https://secretary.raulchiclano.es/enviarinforme"
+        for p in Publicadores.search([]):
+            print(p.telefono, flush=True)
+        
+        password = post.get('password'),
+        if password[0] != '7757':
+            return 'Contraseña incorrecta'
+        print("--------------------->>>>", password, flush=True)
+        return http.request.render("secretary.pendiente", {
+                'grupos': Grupos.search([]),
+                'publicadores': Publicadores,
+                'mensaje' : Mensaje,
+                'publicadoresConInforme': Publicadores_conInforme,
+               })
+
+
+
+
+
---
+++ models/models.py
@@ -0,0 +1,342 @@
+# -*- coding: utf-8 -*-
+
+# Para ver print() en los logs en Docker
+#import logging
+#rom select import select
+
+#_logger = logging.getLogger(__name__)
+#_logger.info('Mensaje')
+# ----------------------------------------
+
+
+from email.policy import default
+import string
+from tokenize import group
+from odoo import models, fields, api, exceptions
+from datetime import *
+
+
+
+def get_years():
+    year_list = []
+    for i in range(2022, 2050):
+        year_list.append((str(i), str(i)))
+    return year_list
+
+class publicadores(models.Model):
+     _name = 'secretary.publicadores'
+     _description = 'Publicadores'
+      
+
+
+     name = fields.Char(string='Nombre completo', default="apellidos, nombre", required=True)
+     fecha_nacimiento = fields.Date(string='Fecha de nacimiento')
+     fecha_bautismo = fields.Date(string='Fecha de bautismo')
+     telefono = fields.Integer(string='Teléfono')
+     genero = fields.Selection([('H','Hombre'), ('M','Mujer')], string='Género')
+     nombramiento = fields.Selection([('A','Anciano'), ('S','Siervo Ministerial')], string='Nombramiento')
+     direccion = fields.Char(string='Dirección')
+     tipo = fields.Selection([('P','Publicador'), ('A', 'Auxiliar'), ('R','Regular')], string='Tipo', required=True)
+     grupo = fields.Many2one(string='Grupo de Servicio', comodel_name="secretary.grupos")
+     email = fields.Char(string='Email')
+     image = fields.Binary(string='Imagen')
+     activo = fields.Boolean(string='Activo', default='True', readonly=True)
+     informe_id = fields.One2many("secretary.informes", "nombre", string="Mis Informes")
+
+
+
+
+
+     def f_activo(self):
+        self.activo = not self.activo
+
+
+
+
+class grupos(models.Model):
+    _name = 'secretary.grupos'
+    _description = 'Grupos de Servicio'
+
+    id = fields.Integer(string= 'Nombre', default=lambda self: self.env['ir.sequence'].next_by_code('increment_your_field'))
+    name = fields.Integer(string='Nombre', related="id")
+    responsable = fields.Many2one('secretary.publicadores', string="Superintendente de Grupo ")
+    def name_get(self):
+        result = []
+        for g in self:
+            name = 'Grupo #%s' % (g.name)
+            result.append((g.id, name))
+        return result
+        
+
+
+
+
+
+class informes(models.Model):
+     _name = 'secretary.informes'
+     _description = 'Informe de predicación'
+     _rec_name = 'fecha'
+
+     #current_year = fields.Integer(string="Año actual", default=datetime.now().year)
+     #current_year = str(current_year)
+      
+     @api.onchange('mes')
+     def _get_publicadores_por_informar(self):
+        print("[*] Fecha-->", self.fecha, flush=True)
+        pub_ya_informado = []
+        if self.fecha != False:
+            pub_con_informe = self.env['secretary.publicadores'].search([('informe_id.fecha','=',self.fecha)])
+            for x in pub_con_informe:
+                pub_ya_informado.append(x.id)
+            print(pub_ya_informado, flush=True)
+            return {'domain':{'nombre':[('id','!=',pub_ya_informado)]}}
+        else:
+            pass
+     def calculoMesAInformar():
+        
+        mes_anterior= str(date.today().month)
+        
+        if mes_anterior == '1':
+           mes_anterior = '12'
+           año = '2022'
+        else:
+            mes_anterior = str((date.today().month)-1)
+        mes = mes_anterior    
+        return mes
+     
+     nombre = fields.Many2one("secretary.publicadores", string='Publicador')
+     tipo_publicador = fields.Selection(string= "Tipo de publicador", related='nombre.tipo')
+     grupo_publicador = fields.Many2one(string= "Grupo de publicador", related='nombre.grupo')# OJO: CAMBIADO RECIENTEMENTE
+     mes = fields.Selection([('1', 'Enero'), ('2', 'Febrero'), ('3', 'Marzo'), ('4', 'Abril'),('5', 'Mayo'), ('6', 'Junio'), ('7', 'Julio'), ('8', 'Agosto'),('9', 'Septiembre'), ('10', 'Octubre'), ('11', 'Noviembre'), ('12', 'Diciembre'), ], string='Mes', default=calculoMesAInformar())
+     current_year = datetime.strftime(datetime.today(),'%Y')
+     fecha = fields.Char(compute="_compute_date", store=True)
+     año = fields.Selection(get_years(), string='Año', default=current_year)
+     horas = fields.Integer(string='Horas', required=True)
+     revisitas = fields.Integer(string="Revisitas")
+     cursos = fields.Integer(string="Cursos Bíblicos")
+     publicaciones = fields.Integer(string="Publicaciones")
+     videos = fields.Integer(string="Videos")
+     revisitas = fields.Integer(string="Revisitas")
+     notas = fields.Text(string='Notas:')
+     este_mes_aux = fields.Boolean(string = "Aux. 30/50 horas", readonly=True) #TODO: Integrarlo cuadno se filtre campos para mayor comodida y estetica
+     tipo_informe = fields.Selection([('P','Publicador'), ('A', 'Auxiliar'), ('R','Regular')], string= "Tipo de Informe", default='P')
+
+                 
+     def este_mes_aux_activo(self):
+        self.este_mes_aux = not self.este_mes_aux     
+
+     @api.depends("mes")
+     def _compute_date(self):
+        for record in self:
+            record.fecha = "%s-%s" %(record.mes,record.año)
+
+     @api.onchange('nombre')
+     def _set_tipo_informe(self):
+        print("Toc,toc", self.nombre.tipo, flush=True)
+        if self.nombre.tipo != False:
+            self.tipo_informe = "%s" %(self.nombre.tipo)
+        else:
+            pass
+
+     @api.constrains('horas')
+     def _validate_horas(self):
+        if self.horas <= 0:
+            raise exceptions.ValidationError('¡CUIDADO: EL valor de las HORAS no debe de ser menor a 0!')
+
+   
+     _sql_constraints = [
+        ('nombre_unique', 'unique(nombre, mes, año)', '¡Parece que ya informaste. Solo puede introducir un informe por publicador! En caso de error, ponte en contacto con el secretario.')
+    ]
+
+class informes_dashboard(models.Model):
+    _name = 'secretary.informes_dashboard'
+    _inherits = {'secretary.informes': 'mes_id'}
+
+    mes_id = fields.Many2one('secretary.informes', ondelete='cascade', string= 'Mes')
+
+
+
+
+# MODELO PARA REPORTE REGISTRO DE PUBLICADOR (S-21).    
+class RegistroPublicador(models.TransientModel):
+    _name = 'secretary.registro_publicador_report'
+    _description = 'Formularios de Registro de publicador de la congregación (S-21)'
+   # _inherit = 'secretary.informes'
+   # _auto = False
+   # _rec_name = 'id'
+    def _año_servicio_sort(self):
+        informes = self.env['secretary.informes'].search([])
+        informes_sorted = informes.sorted(key= lambda i : i.año ,reverse=True)
+        menu_año = []
+        for informe in informes_sorted:
+            menu_año.append((informe.año, informe.año))
+        menu_año = list(dict.fromkeys(menu_año))
+        print(menu_año, flush=True)
+        return menu_año
+
+    año_servicio = fields.Selection(_año_servicio_sort, string= "Seleccione año de servicio a generar reporte")
+   
+    def print_report_formularios_registro_publicador(self):
+        return self.env.ref('secretary.action_registroPublicador_report').report_action(self)
+
+    def get_publicadores_por_año(self):
+        lista_por_año = self.env['secretary.publicadores'].search([('informe_id.año','=',self.año_servicio),])
+        print("Debug: lista_por_año_____________",lista_por_año, flush=True)
+        #for publicador in lista_por_año:
+        #    print("Debug: nombre______", publicador.grupo[0]['name'], flush=True)
+        return lista_por_año
+
+    def lista_meses_con_informes(self):
+        meses = self.env['secretary.informes'].search([('año','=',self.año_servicio)])
+        print("meses---->",meses,flush=True)
+        meses_sorted = meses.sorted(key= lambda i : i.mes ,reverse=True)
+        lista_meses_con_informes = []
+        for mes in meses_sorted:
+            lista_meses_con_informes.append(int(mes.mes))
+        lista_meses_con_informes = list(dict.fromkeys(lista_meses_con_informes))
+        lista_meses_con_informes.sort()
+        print(lista_meses_con_informes, flush=True)
+        return lista_meses_con_informes
+
+    def test(self):
+        lista = [8,9,10,11]
+        for l in lista:
+            self.get_informes_por_tipo(l)
+            
+    def get_informes_por_tipo(self,x):
+        mes=x
+        grouped = self.env['secretary.informes'].read_group(
+                [('fecha', '=', '%s-%s' %(mes,self.año_servicio)),],# WHERE
+                [('horas:sum'),('publicaciones:sum'),('videos:sum'),('revisitas:sum'),('cursos:sum')], # FUNCTION IN SELECT; SELECT SUM (cv) AS total
+                ['tipo_informe'] # GROUPBY
+            )
+        print("mes es --->",mes, flush=True)
+        print("totales por mes--->",grouped, flush=True)
+            
+        return grouped # devuelve array de tuplas
+
+
+
+
+# MODELO PARA REPORTE TOTALES DEL MES       
+class TotalesMensuales(models.TransientModel):
+    _name = 'secretary.totales_mensuales_report'
+    _description = 'Totales mensuales de la predicación'
+   # _inherit = 'secretary.informes'
+   # _auto = False
+   # _rec_name = 'id'
+
+   
+    def _informe_sort(self):
+        informes = self.env['secretary.informes'].search([])
+        informes_sorted = informes.sorted(key= lambda i : i.fecha ,reverse=True)
+        menu_fecha = []
+        for informe in informes_sorted:
+            menu_fecha.append((informe.fecha, informe.fecha))
+        menu_fecha = list(dict.fromkeys(menu_fecha))
+        return menu_fecha
+
+    mes_seleccionado = fields.Selection(_informe_sort, string = "Escoga mes para generar informe", required = True)
+    
+    def print_report_totales_mensuales(self):
+        return self.env.ref('secretary.action_totalesMensuales_report').report_action(self)
+
+    def get_sum_totales_mensuales(self):
+        grouped = self.env['secretary.informes'].read_group(
+            [('fecha', '=', self.mes_seleccionado),],# WHERE
+            [('horas:sum'),('publicaciones:sum'),('videos:sum'),('revisitas:sum'),('cursos:sum')], # FUNCTION IN SELECT; SELECT SUM (cv) AS total
+            ['tipo_informe'] # GROUPBY
+        )
+        print(grouped, flush=True)
+        return grouped # devuelve array de tuplas
+
+    def get_totales(self):
+        total = self.env['secretary.informes'].read_group(
+            [('fecha', '=', self.mes_seleccionado),],
+            [('horas:sum'),('publicaciones:sum'),('videos:sum'),('revisitas:sum'),('cursos:sum')], 
+            ['fecha']
+        )
+        return total
+
+    def get_publicadores_activos(self):
+        p_activos = self.env['secretary.informes'].read_group(
+            [('horas', '>', 0),],
+            [('nombre:array_agg')], 
+            ['fecha']
+        )
+        print(p_activos, flush=True)
+        return self.filtrar_ultimos_meses(p_activos)
+
+    def filtrar_ultimos_meses(self, lista):
+        count = []
+        for i in range(-6, 0):
+            try:
+                valor = lista[i]['nombre']
+            except IndexError:
+                valor = None
+            if valor != None:
+                count.extend(valor)
+        return len(set(count))
+
+    def get_publicadores_irregulares(self):
+        lista_conInforme = self.env['secretary.publicadores'].search([('informe_id.fecha','=',self.mes_seleccionado)])
+        lista_total = self.env['secretary.publicadores'].search([])
+        print("Debug: lista_conInforme_____________",lista_conInforme, flush=True)
+        print("Debug: lista_total_____________",lista_total, flush=True)
+        lista_irregulares = lista_total - lista_conInforme
+        print("Debug: lista_irregulares_____________",lista_irregulares, flush=True)
+
+        for publicador in lista_irregulares:
+            print("Debug: nombre______", publicador.grupo[0]['name'], flush=True)
+        return lista_irregulares
+        
+
+# MODELO PARA REPORTE TOTALES POR GRUPOS DE SERVICIO       
+class TotalesMensuales(models.TransientModel):
+    _name = 'secretary.totales_porgrupo_report'
+    _description = 'Totales por grupo de servicio'
+   # _inherit = 'secretary.informes'
+   # _auto = False
+   # _rec_name = 'id'
+
+   
+    def _grupo_sort(self):
+        grupos = self.env['secretary.grupos'].search([])
+        menu_grupo = []
+        for grupo in grupos:
+            menu_grupo.append((grupo.id, 'Grupo_'+ str(grupo.name)))
+        return menu_grupo
+
+    grupo_seleccionado = fields.Selection(_grupo_sort, string = "Escoga grupo para generar informe", required = True)
+    
+    def print_report_totales_porgrupo(self):
+        return self.env.ref('secretary.action_totalesporGrupo_report').report_action(self)
+
+
+
+    def get_publicadores_porgrupo(self):
+        lista_porgrupos = self.env['secretary.publicadores'].search([('grupo','=',self.grupo_seleccionado),])
+        print("Debug: lista_porgrupos_____________",lista_porgrupos, flush=True)
+        #for publicador in lista_porgrupos:
+        #    print("Debug: nombre______", publicador.grupo[0]['name'], flush=True)
+        return lista_porgrupos
+        
+         
+ 
+# MODELO PARA REPORTE TARJETA DE PUBLICADORES       
+class InformesReport(models.AbstractModel):
+    _name='report.secretary.informe_mensual'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        report_obj = self.env['ir.actions.report']
+        report = report_obj._get_report_from_name('secretary.report_tarjeta_publicador')
+        return {
+            'doc_ids': docids,
+            'doc_model': self.env['secretary.publicadores'],
+            'docs': self.env['secretary.publicadores'].browse(docids)
+        }
+
+
+
+
